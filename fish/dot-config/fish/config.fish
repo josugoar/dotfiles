be17ff038d06410e2744a097415ef834e2ee8fd1
Josu <56826352+josugoar@users.noreply.github.com>
2021-02-21 17:03:02 +0100

    Update config.fish

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -8,29 +8,29 @@ end
 
 function __fish_cancel_commandline
     # Close the pager if it's open (#4298)
-    commandline -f cancel
+    commandline --function cancel
 
-    set -l cmd (commandline)
+    set --local cmd (commandline)
     if test -n "$cmd"
-        commandline -C 1000000
-        if set -q fish_color_cancel
+        commandline --cursor 1000000
+        if set --query fish_color_cancel
             echo -s (set_color $fish_color_cancel) "^C" (set_color normal)
         else
             echo -s "^C"
         end
-        if command -sq tput
+        if command --quiet --search tput
             # Clear to EOL (to erase any autosuggestion).
             echo (tput el; or tput ce)
         end
-        for i in (seq (commandline -L))
-            echo ""
+        for i in (seq (commandline --line))
+            echo
         end
         commandline ""
         emit fish_cancel
     end
     # Repaint even if we haven't cancelled anything,
     # so the prompt refreshes and the terminal scrolls to it.
-    commandline -f repaint
+    commandline --function repaint
 end
 
 set fish_greeting
