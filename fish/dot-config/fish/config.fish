8f577c651ae8ecc0dc6927fcb1c40f6025fb5605
Josu <56826352+josugoar@users.noreply.github.com>
2021-02-21 10:53:05 +0100

    Update config.fish

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -2,6 +2,32 @@ function add_newline --on-event fish_preexec
     echo
 end
 
+function __fish_cancel_commandline
+    # Close the pager if it's open (#4298)
+    commandline -f cancel
+
+    set -l cmd (commandline)
+    if test -n "$cmd"
+        commandline -C 1000000
+        if set -q fish_color_cancel
+            echo -ns (set_color $fish_color_cancel) "^C" (set_color normal)
+        else
+            echo -ns "^C"
+        end
+        if command -sq tput
+            # Clear to EOL (to erase any autosuggestion).
+            echo -n (tput el; or tput ce)
+        end
+        for i in (seq (commandline -L))
+            echo ""
+        end
+        commandline ""
+        commandline -f execute
+        # This is the important line
+        commandline -f repaint
+    end
+end
+
 set fish_greeting
 
 starship init fish | source
