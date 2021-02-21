94e7e1ae6ff07885053fb5d99bc41a4ee9f81d0c
Josu <56826352+josugoar@users.noreply.github.com>
2021-02-21 17:51:50 +0100

    Update config.fish

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -1,36 +1,15 @@
-function add_newline --on-event fish_prompt
+function add_newline_on_cancel --on-event fish_cancel
+    # ADD AS MANY NEW LINES AS NEEDED
+    echo
+    echo
+end
+
+function add_newline_on_prompt --on-event fish_prompt
     if set --query ADD_NEWLINE
         echo
     else
         set --global ADD_NEWLINE
     end
-end
-
-function __fish_cancel_commandline
-    # Close the pager if it's open (#4298)
-    commandline --function cancel
-
-    set --local cmd (commandline)
-    if test -n "$cmd"
-        commandline --cursor 1000000
-        if set --query fish_color_cancel
-            echo -s (set_color $fish_color_cancel) "^C" (set_color normal)
-        else
-            echo -s "^C"
-        end
-        if command --quiet --search tput
-            # Clear to EOL (to erase any autosuggestion).
-            echo (tput el; or tput ce)
-        end
-        for i in (seq (commandline --line))
-            echo
-        end
-        commandline ""
-        emit fish_cancel
-    end
-    # Repaint even if we haven't cancelled anything,
-    # so the prompt refreshes and the terminal scrolls to it.
-    commandline --function repaint
 end
 
 set fish_greeting
