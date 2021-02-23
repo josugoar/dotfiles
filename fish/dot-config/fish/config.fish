60ac03e542e3b291cd6850bb0b462a178acc0698
Josu <56826352+josugoar@users.noreply.github.com>
2021-02-23 14:44:20 +0100

    Update config.fish

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -1,23 +1,18 @@
+function __add_newline \
+        --on-event fish_cancel \
+        --on-event fish_postexec
+    echo
+end
+
 function __line_break --on-event fish_cancel
-    if commandline --quiet tput
+    if command --quiet tput
         tput ed
     end
 
-    for i in (seq 2 (count (fish_prompt)))
+    for i in (fish_prompt)[2..-1]
         echo
     end
-
-    emit fish_prompt
 end
-
-function __add_newline --on-event fish_prompt
-    if set --query _add_newline_on_prompt
-        echo
-    else
-        set --global _add_newline_on_prompt
-    end
-end
-
 
 set fish_greeting
 
