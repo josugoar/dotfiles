287db0120f9aa7d0cac653af19fcabc99f148bfa
Josu <josugoar@gmail.com>
2023-08-10 16:30:52 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo dnf install fish
+echo /usr/local/bin/fish | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/fish
+
+git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git ~/nerd-fonts
+~/nerd-fonts/install.sh
+
+curl -sS https://starship.rs/install.sh | sh
