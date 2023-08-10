2d7a64fcfc018b39266439b4fca11e25a98b5164
Josu <josugoar@gmail.com>
2023-08-10 16:01:18 +0200

    Update config.fish

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -4,13 +4,6 @@ function __add_newline --on-event fish_prompt
     end
 end
 
-function __line_break --on-event fish_cancel
-    tput ed
-    for i in (fish_prompt)[..-2]
-        echo
-    end
-end
-
 set -g fish_greeting
 
 starship init fish | source
