274917115e45af7fa690e6a4779167e5c662d846
Josu <josugoar@gmail.com>
2023-08-13 19:21:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,15 @@ sudo dnf install fish
 echo /usr/local/bin/fish | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish
 
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
 sudo dnf install nano
 
-git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git ~/nerd-fonts
-~/nerd-fonts/install.sh
+git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git ~/.nerd-fonts
+~/.nerd-fonts/install.sh
+
 curl -sS https://starship.rs/install.sh | sh
 
 sudo dnf install stow
-stow --target ~ --dotfiles code fish gtk3 nano starship
+stow --target "$HOME" --dotfiles code fish gtk3 nano starship
