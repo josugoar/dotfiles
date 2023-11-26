0781654402d1ec9b70eeef441fff6e6091e49add
Josu <josugoar@gmail.com>
2023-11-26 22:30:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-dnf check-update
 sudo dnf install code
 
 sudo dnf install fish
@@ -20,4 +19,4 @@ git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git ~/.nerd-fonts
 curl -sS https://starship.rs/install.sh | sh
 
 sudo dnf install stow
-stow --target "$HOME" --dotfiles code fish gtk3 nano starship
+stow --dotfiles --target "$HOME" code dconf firefox fish gtk3 nano starship
