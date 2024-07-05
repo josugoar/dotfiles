0d0a7e138c88d2ffa4d74b23488b441bc0228964
Josu <josugoar@gmail.com>
2024-07-05 14:57:31 +0200

    Don't display first newline

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -1,5 +1,7 @@
-function __add_newline --on-event fish_cancel --on-event fish_prompt
-    echo
+function __add_newline --on-event fish_prompt
+    function __add_newline --on-event fish_prompt
+        echo
+    end
 end
 
 set -g fish_greeting
