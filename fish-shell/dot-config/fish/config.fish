d9660dd36b5ac836058a80a24a6fa87c1145a196
Josu <josugoar@gmail.com>
2024-07-07 21:14:35 +0200

    Update config.fish

--- fish-shell/dot-config/fish/config.fish
+++ fish-shell/dot-config/fish/config.fish
@@ -1,9 +1,9 @@
 set -g fish_greeting
 
 function __add_newline --on-event fish_cancel --on-event fish_prompt
-    function __add_newline --on-event fish_cancel --on-event fish_prompt
-        echo
-    end
+  function __add_newline --on-event fish_cancel --on-event fish_prompt
+    echo
+  end
 end
 
 starship init fish | source
@@ -17,7 +17,7 @@ set -g fish_cursor_external line
 set -g fish_key_bindings fish_vi_key_bindings
 
 function __last_history_item
-    echo $history[1]
+  echo $history[1]
 end
 
 abbr --add !! --position anywhere --function __last_history_item
