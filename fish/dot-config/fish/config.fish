fdc0688194403bd23f868894a4ae8460f1cb40f2
Josu <josugoar@gmail.com>
2024-07-07 18:43:00 +0200

    Update config.fish

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -1,10 +1,17 @@
+set -g fish_greeting
+
 function __add_newline --on-event fish_cancel --on-event fish_prompt
     function __add_newline --on-event fish_cancel --on-event fish_prompt
         echo
     end
 end
 
-set -g fish_greeting
-set -g fish_key_bindings fish_vi_key_bindings
-
 starship init fish | source
+
+set -g fish_cursor_default block
+set -g fish_cursor_insert line
+set -g fish_cursor_replace_one underscore
+set -g fish_cursor_replace underscore
+set -g fish_cursor_external line
+
+set -g fish_key_bindings fish_vi_key_bindings
