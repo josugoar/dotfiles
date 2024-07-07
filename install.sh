b814b10a112fbead8fafc2c6dab84bd901f0da44
Josu <josugoar@gmail.com>
2024-07-07 21:31:18 +0200

    Install zoxide

--- install.sh
+++ install.sh
@@ -45,6 +45,9 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf --assumeyes install code
 
+# zoxide
+sudo dnf --assumeyes install zoxide
+
 # stow
 sudo dnf --assumeyes install stow
 stow --dotfiles --target "$HOME" dconf firefox fish-shell gtk nano neovim starship vscode
