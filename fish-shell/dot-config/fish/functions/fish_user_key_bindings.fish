a2edda0772c139802b09eafd8904d4456de2ba8c
Josu <josugoar@gmail.com>
2024-07-07 21:14:16 +0200

    Update fish_user_key_bindings.fish

--- fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
+++ fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,8 @@
 function fish_user_key_bindings
-    fish_default_key_bindings --mode insert
-    fish_vi_key_bindings --no-erase
+  fish_default_key_bindings --mode insert
+  fish_vi_key_bindings --no-erase
 
-    fzf --fish | source
+  bind --mode insert jj "set fish_bind_mode default; commandline --function backward-char repaint;"
+
+  fzf --fish | source
 end
