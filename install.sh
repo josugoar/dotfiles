65a3319a3327e7d7a56d744617bcf7b5e46c391b
josugoar <josugoar@gmail.com>
2024-07-07 19:24:37 +0200

    Rename dirs

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# vscode
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf --assumeyes install code
-
 # fd
 sudo dnf --assumeyes install fd-find
 
@@ -39,6 +34,11 @@ sudo dnf --assumeyes install ripgrep
 # starship
 curl --show-error --silent https://starship.rs/install.sh | sudo sh -s -- --yes
 
+# vscode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf --assumeyes install code
+
 # stow
 sudo dnf --assumeyes install stow
-stow --dotfiles --target "$HOME" vscode dconf firefox fish-shell gtk nano neovim starship
+stow --dotfiles --target "$HOME" dconf firefox fish-shell gtk nano neovim starship vscode
