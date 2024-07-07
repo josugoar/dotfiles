f43baa886fffb8443781b3ebaa6f971b1d36fb76
Josu <josugoar@gmail.com>
2024-07-07 21:19:33 +0200

    Fix unterminated string

--- fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
+++ fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
@@ -2,7 +2,7 @@ function fish_user_key_bindings
   fish_default_key_bindings --mode insert
   fish_vi_key_bindings --no-erase
 
-  bind --mode insert jj "if commandline --paging-mode; commandline --function cancel; else; set fish_bind_mode default; commandline --function backward-char repaint; end
+  bind --mode insert jj "if commandline --paging-mode; commandline --function cancel; else; set fish_bind_mode default; commandline --function backward-char repaint; end"
 
   fzf --fish | source
 end
