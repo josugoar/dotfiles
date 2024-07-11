d437b7d9e8226d863ea84e0529885633376a63c7
Josu <josugoar@gmail.com>
2024-07-12 00:54:36 +0200

    Refactor

--- fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
+++ fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,3 @@
 function fish_user_key_bindings
-  bind --mode insert jj "if commandline --paging-mode; commandline --function cancel; else; set fish_bind_mode default; commandline --function backward-char repaint; end"
+    bind --mode insert jj "if commandline --paging-mode; commandline --function cancel; else; set fish_bind_mode default; commandline --function backward-char repaint; end"
 end
