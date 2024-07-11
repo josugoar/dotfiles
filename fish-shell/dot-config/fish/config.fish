fe5296e6de7fa8613399379185b0bd3b6e97f6e1
Josu <josugoar@gmail.com>
2024-07-11 15:45:21 +0200

    Update config.fish

--- fish-shell/dot-config/fish/config.fish
+++ fish-shell/dot-config/fish/config.fish
@@ -1,21 +1,3 @@
-function __add_newline --on-event fish_cancel --on-event fish_prompt
-  function __add_newline --on-event fish_cancel --on-event fish_prompt
-    echo
-  end
-end
-starship init fish | source
+fish_add_path --global "$HOME/.local/bin" "$HOME/bin"
 
-set --global fish_cursor_default block
-set --global fish_cursor_insert line
-set --global fish_cursor_replace_one underscore
-set --global fish_cursor_replace underscore
-set --global fish_cursor_external line
-set --global fish_cursor_visual block
-set --global fish_key_bindings fish_vi_key_bindings
-
-zoxide init --cmd cd fish | source
-
-function __last_history_item
-  echo $history[1]
-end
-abbr --add !! --position anywhere --function __last_history_item
+set --global SYSTEMD_PAGER
