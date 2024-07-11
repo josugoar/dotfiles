e0c3574caf1ca7d45c517a953dda05198aecea9f
Josu <josugoar@gmail.com>
2024-07-11 15:43:13 +0200

    Update fish_user_key_bindings.fish

--- fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
+++ fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
@@ -1,8 +1,3 @@
 function fish_user_key_bindings
-  fish_default_key_bindings --mode insert
-  fish_vi_key_bindings --no-erase
-
   bind --mode insert jj "if commandline --paging-mode; commandline --function cancel; else; set fish_bind_mode default; commandline --function backward-char repaint; end"
-
-  fzf --fish | source
 end
