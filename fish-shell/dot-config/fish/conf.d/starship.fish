ea34988a0cd754779b7c457a436fb3d8a7061105
Josu <josugoar@gmail.com>
2024-07-12 00:54:22 +0200

    Refactor

--- fish-shell/dot-config/fish/conf.d/starship.fish
+++ fish-shell/dot-config/fish/conf.d/starship.fish
@@ -1,7 +1,7 @@
 function starship_add_newline --on-event fish_cancel --on-event fish_prompt
-  function starship_add_newline --on-event fish_cancel --on-event fish_prompt
-    echo
-  end
+    function starship_add_newline --on-event fish_cancel --on-event fish_prompt
+        echo
+    end
 end
 
 starship init fish | source
