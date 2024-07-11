05264ea751d98bcd87192c291affbbce174b5b57
Josu <josugoar@gmail.com>
2024-07-11 15:46:02 +0200

    Update starship.fish

--- fish-shell/dot-config/fish/conf.d/starship.fish
+++ fish-shell/dot-config/fish/conf.d/starship.fish
@@ -1,5 +1,5 @@
-function __add_newline --on-event fish_cancel --on-event fish_prompt
-  function __add_newline --on-event fish_cancel --on-event fish_prompt
+function starship_add_newline --on-event fish_cancel --on-event fish_prompt
+  function starship_add_newline --on-event fish_cancel --on-event fish_prompt
     echo
   end
 end
