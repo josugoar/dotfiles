b53e20c7995d351dda4484ef72de96b3802bae9a
Josu <josugoar@gmail.com>
2024-07-12 17:10:53 +0200

    Move keybinding to correct place

--- fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
+++ fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,4 @@
 function fish_user_key_bindings
+    bind --erase \cr
     bind --mode insert jj "if commandline --paging-mode; commandline --function cancel; else; set fish_bind_mode default; commandline --function backward-char repaint; end"
 end
