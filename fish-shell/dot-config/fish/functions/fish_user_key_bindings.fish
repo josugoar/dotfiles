4ee2302e237a895a8895075acad21ca825428526
Josu <josugoar@gmail.com>
2024-07-13 02:25:15 +0200

    Add checks

--- fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
+++ fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,14 @@
 function fish_user_key_bindings
-    bind --erase \cr
-    bind --mode insert jj "if commandline --paging-mode; commandline --function cancel; else; set fish_bind_mode default; commandline --function backward-char repaint; end"
+    if contains insert (bind --list-modes)
+        if functions --query fzf_key_bindings
+            bind --erase \cr
+        end
+
+        bind --mode insert jj "if commandline --paging-mode
+                                   commandline --function cancel
+                               else
+                                   set fish_bind_mode default
+                                   commandline --function backward-char repaint
+                               end"
+    end
 end
