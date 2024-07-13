0f7e7d9eb6a535788493b0c2878d6d5c43e93bd0
Josu <josugoar@gmail.com>
2024-07-13 02:45:56 +0200

    Delete fish-shell/dot-config/fish/conf.d/fish_cursor.fish

--- fish-shell/dot-config/fish/conf.d/fish_cursor.fish
+++
@@ -1,6 +0,0 @@
-set --global fish_cursor_default block
-set --global fish_cursor_insert line
-set --global fish_cursor_replace_one underscore
-set --global fish_cursor_replace underscore
-set --global fish_cursor_external line
-set --global fish_cursor_visual block
