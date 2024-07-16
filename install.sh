38ef62dffbf5193ab8929855e28b7ceded1cca83
Josu <56826352+josugoar@users.noreply.github.com>
2024-07-16 15:43:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,48 +1,54 @@
 #!/bin/sh
 
-## gnome-themes-extra
-sudo dnf --quiet --assumeyes install gnome-themes-extra
+set -e
+
+# fd
+dnf install fd-find
 
 # fish-shell
-sudo dnf --quiet --assumeyes install fish
+dnf install fish
 chsh --shell /usr/bin/fish
 
-## fzf
-sudo dnf --quiet --assumeyes install fzf
+# fzf
+dnf install fzf
 
-## zoxide
-sudo dnf --quiet --assumeyes install zoxide
+# gnome-shell-extensions
+dnf install gnome-shell-extension-launch-new-instance
+
+# gnome-themes-extra
+dnf install gnome-themes-extra
+
+# fira-code
+dnf install fira-code-fonts
+
+# lazygit
+dnf copr enable atim/lazygit
+dnf install lazygit
 
 # neovim
-sudo dnf --quiet --assumeyes install neovim
+dnf install neovim
 
-## fd
-sudo dnf --quiet --assumeyes install fd-find
-
-## lazygit
-sudo dnf --quiet --assumeyes copr enable atim/lazygit
-sudo dnf --quiet --assumeyes install lazygit
-
-## nerd-fonts
+# nerd-fonts
 git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git ~/.nerd-fonts
 ~/.nerd-fonts/install.sh NerdFontsSymbolsOnly
 
-## ripgrep
-sudo dnf --quiet --assumeyes install ripgrep
+# ripgrep
+dnf install ripgrep
 
 # starship
-curl --show-error --silent https://starship.rs/install.sh | sudo sh -s -- --yes
+curl https://starship.rs/install.sh | sh
+
+# tiling-assistant
+curl https://github.com/Leleat/Tiling-Assistant/releases/latest/download/tiling-assistant@leleat-on-github.shell-extension.zip | unzip -d ~/.local/share/gnome-shell/extensions/tiling-assistant@leleat-on-github
 
 # vscode
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf --assumeyes install code
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf install code
 
-# fira-code-fonts
-# gnome-shell-extension-launch-new-instance
-
-# curl https://github.com/Leleat/Tiling-Assistant/releases/latest/download/tiling-assistant@leleat-on-github.shell-extension.zip | unzip -d ~/.local/share/gnome-shell/extensions/tiling-assistant@leleat-on-github
+# zoxide
+dnf install zoxide
 
 # stow
-sudo dnf --quiet --assumeyes install stow
-stow --dotfiles --target "$HOME" dconf firefox fish-shell gtk nano neovim starship twp ublock vscode
+sudo dnf install stow
+stow --dotfiles --target "$HOME" dconf firefox fish-shell fontconfig git gtk nano neovim starship twp ublock vscode
