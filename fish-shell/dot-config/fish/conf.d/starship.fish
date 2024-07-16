2e8ea195610621185abc125b3bc1d5485ce773be
Josu <56826352+josugoar@users.noreply.github.com>
2024-07-16 20:04:26 +0200

    Fix upstream transient prompt

--- fish-shell/dot-config/fish/conf.d/starship.fish
+++ fish-shell/dot-config/fish/conf.d/starship.fish
@@ -4,10 +4,19 @@ end
 
 starship init fish | source
 
-enable_transience
-
 function __starship_add_newline --on-event fish_cancel --on-event fish_prompt
     function __starship_add_newline --on-event fish_cancel --on-event fish_prompt
         echo
     end
+end
+
+enable_transience
+
+function reset-transient --on-event fish_prompt
+    set --global TRANSIENT 0
+end
+
+function transient_execute
+    set --global TRANSIENT 1
+    commandline --function repaint execute
 end
