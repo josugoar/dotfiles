7fb4a84922e509f93f6c2977b2e63d452aae4bcd
Josu <56826352+josugoar@users.noreply.github.com>
2024-07-17 01:14:42 +0200

    Update fish_user_key_bindings.fish

--- fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
+++ fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
@@ -7,7 +7,7 @@ function fish_user_key_bindings
     end
 
     if contains insert $modes
-        if builtin functions --query fzf_key_bindings
+        if functions --query fzf_key_bindings
             bind -e \cr
         end
 
