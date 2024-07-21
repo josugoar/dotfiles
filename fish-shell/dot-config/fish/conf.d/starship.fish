709f845d434a4b6791c7148387ddccb30c939bb6
Josu <56826352+josugoar@users.noreply.github.com>
2024-07-21 16:00:44 +0200

    Update starship.fish

--- fish-shell/dot-config/fish/conf.d/starship.fish
+++ fish-shell/dot-config/fish/conf.d/starship.fish
@@ -4,7 +4,7 @@ end
 
 starship init fish | source
 
-function add_newline --on-event fish_prompt
+function add_newline --on-event fish_posterror --on-event fish_prompt
     if test "$NEWLINE" = "1"
         printf \n
     end
@@ -12,12 +12,12 @@ function add_newline --on-event fish_prompt
 end
 
 function starship_transient_prompt_func
-    if commandline --is-valid && test -n (commandline --current-buffer | string trim -l | string collect)
+    if commandline --is-valid || test $status -eq 1 && test -n (commandline --current-buffer | string trim -l | string collect)
         starship module character    
     end
 end
 
-function reset-transient --on-event fish_cancel --on-event fish_prompt
+function reset-transient --on-event fish_cancel --on-event fish_posterror --on-event fish_prompt
     set -g TRANSIENT 0
 end
 
@@ -27,7 +27,7 @@ function transient_execute
         commandline -f accept-autosuggestion
         return
     end
-    if commandline --is-valid || test -z (commandline --current-buffer | string trim -l | string collect)
+    if commandline --is-valid || test $status -eq 1 || test -z (commandline --current-buffer | string trim -l | string collect)
         set -g TRANSIENT 1
         commandline -f repaint
     else
