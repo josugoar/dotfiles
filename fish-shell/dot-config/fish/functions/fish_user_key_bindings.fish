db4bad3cf98bbe3a352108ebbb82c239e517b038
Josu <56826352+josugoar@users.noreply.github.com>
2024-07-22 20:09:29 +0200

    Use correct cancel behaviour

--- fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
+++ fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
@@ -1,12 +1,11 @@
 function fish_user_key_bindings
-    if test (fish_vercmp $version 3.7.0) -ne 1
-        bind \cc 'commandline -r ""'
-        if contains $fish_key_bindings fish_vi_key_bindings fish_hybrid_key_bindings
-            bind \cc '
-                commandline -r ""
-                set fish_bind_mode insert
-                commandline -f repaint-mode
-            '
-        end
+    set -l cancel_commandline '
+        commandline --replace --current-buffer ""
+        emit fish_cancel
+    '
+    bind \cc $cancel_commandline
+    if contains $fish_key_bindings fish_vi_key_bindings fish_hybrid_key_bindings
+        bind -M insert \cc $cancel_commandline
+        bind -m insert \cc $cancel_commandline
     end
 end
