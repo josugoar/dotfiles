8c30a6da432a979f7574e7f342977bec708b13b8
Josu <56826352+josugoar@users.noreply.github.com>
2024-07-22 03:41:29 +0200

    Add todo

--- fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
+++ fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,7 @@
 function fish_user_key_bindings
     if test (fish_vercmp $version 3.7.0) -ne 1
         bind \cc 'commandline -r ""'
+        # TODO: vi binding (change to insert mode)
     end
 
     if contains $fish_key_bindings fish_vi_key_bindings fish_hybrid_key_bindings
