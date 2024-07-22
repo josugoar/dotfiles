bce084d780f0bc92ded090b9d02a92001132315d
Josu <56826352+josugoar@users.noreply.github.com>
2024-07-22 03:59:17 +0200

    Correct modern default behaviour

--- fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
+++ fish-shell/dot-config/fish/functions/fish_user_key_bindings.fish
@@ -1,7 +1,13 @@
 function fish_user_key_bindings
     if test (fish_vercmp $version 3.7.0) -ne 1
         bind \cc 'commandline -r ""'
-        # TODO: vi binding (change to insert mode)
+        if contains $fish_key_bindings fish_vi_key_bindings fish_hybrid_key_bindings
+            bind \cc '
+                commandline -r ""
+                set fish_bind_mode insert
+                commandline -f repaint-mode
+            '
+        end
     end
 
     if contains $fish_key_bindings fish_vi_key_bindings fish_hybrid_key_bindings
