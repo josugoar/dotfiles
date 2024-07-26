5c3ec468c05da37d4bb1b90359e1bdadb2f5c920
Josu <56826352+josugoar@users.noreply.github.com>
2024-07-26 23:36:13 +0200

    Remove launch new instance

--- install.sh
+++ install.sh
@@ -15,9 +15,6 @@ dnf install fzf
 # gnome-fuzzy-app-search
 curl https://extensions.gnome.org/extension-data/gnome-fuzzy-app-searchgnome-shell-extensions.Czarlie.gitlab.com.v24.shell-extension.zip | unzip -d ~/.local/share/gnome-shell/extensions/gnome-fuzzy-app-search@gnome-shell-extensions.Czarlie.gitlab.com
 
-# gnome-shell-extensions
-dnf install gnome-shell-extension-launch-new-instance
-
 # gnome-themes-extra
 dnf install gnome-themes-extra
 
