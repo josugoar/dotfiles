5da2e1d9275211296072e4c5743dfba36eb89ed2
Josu <56826352+josugoar@users.noreply.github.com>
2024-08-11 16:56:38 +0200

    Add rpmfusion

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf update @core
+dnf swap ffmpeg-free ffmpeg --allowerasing
+dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+
 # dconf
 echo service-db:keyfile/user >>/etc/dconf/profile/user
 
