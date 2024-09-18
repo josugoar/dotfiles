728eb3bd38044d2c3739e74e3c0a6827a29dbc1d
Josu <56826352+josugoar@users.noreply.github.com>
2024-09-18 18:54:45 +0200

    Add extensions and refactor

--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@
 
 set -e
 
+# rpmfusion
 dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf update @core
-dnf swap ffmpeg-free ffmpeg --allowerasing
 
 # adw-gtk3
 dnf install adw-gtk3-theme
@@ -15,11 +14,21 @@ echo service-db:keyfile/user >>/etc/dconf/profile/user
 # fd
 dnf install fd-find
 
+# ffmpeg
+dnf update @core
+dnf swap ffmpeg-free ffmpeg --allowerasing
+
 # fish-shell
 dnf install fish
 
 # fzf
 dnf install fzf
+
+# gnome-shell-extension-appindicator
+curl https://extensions.gnome.org/extension-data/appindicatorsupportrgcjonas.gmail.com.v59.shell-extension.zip | unzip -d ~/.local/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com
+
+# gnome-shell-extension-caffeine
+curl https://extensions.gnome.org/extension-data/caffeinepatapon.info.v55.shell-extension.zip | unzip -d ~/.local/share/gnome-shell/extensions/caffeine@patapon.info
 
 # lazygit
 dnf copr enable atim/lazygit
@@ -29,10 +38,13 @@ dnf install lazygit
 dnf install neovim
 
 # nerd-fonts
-curl --location https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip | unzip -d ~/.local/share/fonts/FiraCode
+curl --location https://github.com/ryanoasis/nerd-fonts/releases/latest/download/NerdFontsSymbolsOnly.zip | unzip -d ~/.local/share/fonts/NerdFontsSymbolsOnly
 
 # power-profile-switcher
 curl https://extensions.gnome.org/extension-data/power-profile-switchereliapasquali.github.io.v19.shell-extension.zip | unzip -d ~/.local/share/gnome-shell/extensions/power-profile-switcher@eliapasquali.github.io
+
+# dash-to-dock
+curl https://extensions.gnome.org/extension-data/dash-to-dockmicxgx.gmail.com.v96.shell-extension.zip | unzip -d ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com
 
 # ripgrep
 dnf install ripgrep
