823bad6dd88ba28e95a667ca140136f2f3ece4d8
Josu <56826352+josugoar@users.noreply.github.com>
2024-09-28 13:07:31 +0200

    Use correct extensioins

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-set -e
-
 # adw-gtk3
 dnf install adw-gtk3-theme
+
+# dash-to-dock
+curl https://extensions.gnome.org/extension-data/dash-to-dockmicxgx.gmail.com.v96.shell-extension.zip | unzip -d ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com
 
 # dconf
 echo service-db:keyfile/user >>/etc/dconf/profile/user
@@ -20,12 +21,12 @@ dnf install fzf
 # gnome-shell-extension-appindicator
 curl https://extensions.gnome.org/extension-data/appindicatorsupportrgcjonas.gmail.com.v59.shell-extension.zip | unzip -d ~/.local/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com
 
-# gnome-shell-extension-caffeine
-curl https://extensions.gnome.org/extension-data/caffeinepatapon.info.v55.shell-extension.zip | unzip -d ~/.local/share/gnome-shell/extensions/caffeine@patapon.info
-
 # lazygit
 dnf copr enable atim/lazygit
 dnf install lazygit
+
+# legacy-theme-auto-switcher-gnome-extension
+curl https://extensions.gnome.org/extension-data/legacyschemeautoswitcherjoshimukul29.gmail.com.v9.shell-extension.zip | unzip -d ~/.local/share/gnome-shell/extensions/legacyschemeautoswitcher@joshimukul29.gmail.com
 
 # neovim
 dnf install neovim
@@ -33,17 +34,14 @@ dnf install neovim
 # nerd-fonts
 curl --location https://github.com/ryanoasis/nerd-fonts/releases/latest/download/NerdFontsSymbolsOnly.zip | unzip -d ~/.local/share/fonts/NerdFontsSymbolsOnly
 
-# power-profile-switcher
-curl https://extensions.gnome.org/extension-data/power-profile-switchereliapasquali.github.io.v19.shell-extension.zip | unzip -d ~/.local/share/gnome-shell/extensions/power-profile-switcher@eliapasquali.github.io
-
-# dash-to-dock
-curl https://extensions.gnome.org/extension-data/dash-to-dockmicxgx.gmail.com.v96.shell-extension.zip | unzip -d ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com
-
 # ripgrep
 dnf install ripgrep
 
 # starship
 curl https://starship.rs/install.sh | sh
+
+# stow
+dnf install stow
 
 # tiling-assistant
 curl https://extensions.gnome.org/extension-data/tiling-assistantleleat-on-github.v47.shell-extension.zip | unzip -d ~/.local/share/gnome-shell/extensions/tiling-assistant@leleat-on-github
@@ -55,6 +53,4 @@ dnf install code
 # zoxide
 dnf install zoxide
 
-# stow
-sudo dnf install stow
 stow --dotfiles --target "$HOME" bash darkreader dconf firefox fish-shell git gtk nano neovim sidebery starship twp ublock vscode
