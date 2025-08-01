af37fc73b51756c49ca5d965b28321e95ccdeb49
Josu <56826352+josugoar@users.noreply.github.com>
2025-08-01 22:14:05 +0200

    Refactor to use new syntax

--- fish-shell/dot-config/fish/conf.d/fzf.fish
+++ fish-shell/dot-config/fish/conf.d/fzf.fish
@@ -3,6 +3,5 @@ if not status is-interactive
 end
 
 fzf --fish | FZF_ALT_C_COMMAND= source
-
-bind -e \cr
-bind -e -M insert \cr
+bind -e ctrl-r
+bind -e -M insert ctrl-r
--- fish-shell/dot-config/fish/functions/up-or-search.fish
+++ fish-shell/dot-config/fish/functions/up-or-search.fish
@@ -1,6 +1,6 @@
 function up-or-search -d "Search back or move cursor up 1 line"
     if commandline --search-mode
-        commandline -f $search_func
+        commandline -f $search_mode
         return
     end
 
@@ -14,11 +14,12 @@ function up-or-search -d "Search back or move cursor up 1 line"
     switch $lineno
         case 1
             if test -z (commandline --current-buffer | string collect)
-                set -g search_func history-search-backward
+                set -g search_mode history-search-backward
+                commandline -f history-search-backward
             else
-                set -g search_func history-prefix-search-backward
+                set -g search_mode history-prefix-search-backward
+                commandline -f history-prefix-search-backward
             end
-            commandline -f $search_func
 
         case '*'
             commandline -f up-line
