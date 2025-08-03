1f34ece6ac3532c0d24657064f38b68c39e13fa0
Josu <56826352+josugoar@users.noreply.github.com>
2025-08-03 16:47:58 +0200

    Ignore assets correctly

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for source_file in $(find * -mindepth 1 -not -name dotfiles.gif -type f -printf "%P\0" | xargs --null); do
+for source_file in $(find * -mindepth 1 -not -wholename "assets/*" -type f -printf "%P\0" | xargs --null); do
   target_file="$HOME/$source_file"
   mkdir --parents "$(dirname "$target_file")"
   ln --force --symbolic "$source_file" "$target_file"
