9bca3476d4d4c6ea7994bd1ed9b0a798946380d2
Josu <56826352+josugoar@users.noreply.github.com>
2025-08-20 14:02:14 +0200

    Finally fix zoxide completion overriding prompt, now watch out for new clean-commandline function in newer fish

--- fish/.config/fish/conf.d/zoxide.fish
+++ fish/.config/fish/conf.d/zoxide.fish
@@ -3,3 +3,18 @@ if not status is-interactive
 end
 
 zoxide init --cmd cd fish | source
+
+function __zoxide_z_complete
+    set -l tokens (builtin commandline --current-process --tokenize)
+    set -l curr_tokens (builtin commandline --cut-at-cursor --current-process --tokenize)
+
+    if test (builtin count $tokens) -le 2 -a (builtin count $curr_tokens) -eq 1
+        complete --do-complete "'' "(builtin commandline --cut-at-cursor --current-token) | string match --regex -- '.*/$'
+    else if test (builtin count $tokens) -eq (builtin count $curr_tokens)
+        set -l query $tokens[2..-1]
+        set -l result (command zoxide query --exclude (__zoxide_pwd) --interactive -- $query 2>/dev/null)
+        and __zoxide_cd $result
+        and builtin commandline --function cancel-commandline
+        builtin commandline --function repaint
+    end
+end
