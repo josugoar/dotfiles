9d2ad8d76eba493bf70fd47884bca116ab1ccc86
Josu <56826352+josugoar@users.noreply.github.com>
2025-08-30 10:26:27 +0200

    Add line break handling

--- fish-shell/.config/fish/conf.d/starship.fish
+++ fish-shell/.config/fish/conf.d/starship.fish
@@ -4,11 +4,20 @@ end
 
 starship init fish | source
 
+stty rows (math $LINES - (fish_prompt | count))
+
+function line_break --on-signal WINCH
+    if test "$LINE_BREAK" = 1
+        stty rows (math $LINES - (fish_prompt | count))
+    end
+    set -g LINE_BREAK 1
+end
+
 function add_newline --on-event fish_posterror --on-event fish_prompt
-    if test "$NEWLINE" = "1"
+    if test "$ADD_NEWLINE" = "1"
         printf \n
     end
-    set -g NEWLINE 1
+    set -g ADD_NEWLINE 1
 end
 
 function starship_transient_prompt_func
